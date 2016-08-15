ab45cf87616759744146bec0c21a298350a89792
X Coan <xjcoan@gmail.com>
2016-08-15 14:19:52 -0500

    update and destroy blogs.

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -1,8 +1,33 @@
 class BlogpostsController < ApplicationController
-  # creates new blog post
+  # creates route for new post.
   def new
     @page_title = "New Blog"
+    @blogpost = Blogpost.new
   end
+
+  # creates route for editing a post
+  def edit
+    @blogpost = Blogpost.find(params[:id])
+  end
+
+  # U action. Renders routes for editing post
+  def update
+    @blogpost = Blogpost.find(params[:id])
+
+    if @blogpost.update(blog_params)
+      redirect_to @blogpost
+    else
+      render 'edit'
+    end
+  end
+
+  # D action.
+  def destroy
+    @blogpost = Blogpost.find(params[:id])
+    @blogpost.destroy
+
+    redirect_to blogposts_path
+end
 
   # lists all posts
   def index
@@ -16,12 +41,15 @@ class BlogpostsController < ApplicationController
     @blogpost = Blogpost.find(params[:id])
   end
 
-  # stores new post in Postgres
+  # C action. Renders route defined in new
   def create
     @blogpost = Blogpost.new(blog_params)
 
-    @blogpost.save
-    redirect_to @blogpost
+    if @blogpost.save
+      redirect_to @blogpost
+    else
+      render 'new'
+    end
   end
 
   private
--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -1,2 +1,3 @@
 class Blogpost < ApplicationRecord
+  validates :title, presence: true
 end
