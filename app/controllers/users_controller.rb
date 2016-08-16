65e0d4df4dfc84ff4c508a28daebc5fb525d3146
X Coan <xjcoan@gmail.com>
2016-08-15 22:18:40 -0500

    Add Users. Only allow 1. Only they can update and destroy blog.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def count
+    @count = User.count
+    if @count != 1
+      return true
+    else
+      return false
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/blogposts'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end
